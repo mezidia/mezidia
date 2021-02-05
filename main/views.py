e4cf4e18da9d64fb19c411b389f0a57f2325c191
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-02-05 22:07:25 +0200

    Improve templates

--- main/views.py
+++ main/views.py
@@ -2,4 +2,39 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    nav_data = [
+        {
+            'id': '#about',
+            'text': 'About',
+        },
+        {
+            'id': '#projects',
+            'text': 'Projects',
+        },
+        {
+            'id': '#members',
+            'text': 'Members',
+        },
+        {
+            'id': '#skills',
+            'text': 'Skills',
+        },
+        {
+            'id': '#interests',
+            'text': 'Interests',
+        },
+        {
+            'id': '#awards',
+            'text': 'Awards',
+        },
+        {
+            'id': '#newMembers',
+            'text': 'New members',
+        },
+
+    ]
+
+    context = {
+        'nav_data': nav_data
+    }
+    return render(request, 'main/index.html', context)
