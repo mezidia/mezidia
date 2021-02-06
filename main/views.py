0143a6483cacf5822298d1c02d2e79ad7afd38ac
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-02-06 09:59:22 +0200

    Get API information

--- main/views.py
+++ main/views.py
@@ -1,4 +1,47 @@
 from django.shortcuts import render
+import requests
+
+
+def get_org_repos(org_name: str) -> list:
+    """
+    Get data about repositories in organization
+
+    :param org_name: str
+    :return: data: list
+    """
+    data = []
+    url = f'https://api.github.com/orgs/{org_name}/repos'
+    repos = requests.get(url)
+    for repo in repos.json():
+        data.append({
+            'name': repo['name'],
+            'url': repo['html_url'],
+            'desc': repo['description'],
+            'time': repo['created_at'],
+        })
+    return data
+
+
+def get_org_members(org_name: str) -> list:
+    """
+    Get data about members in organization
+
+    :param org_name: str
+    :return: data: list
+    """
+    data = []
+    url = f'https://api.github.com/orgs/{org_name}/members'
+    members = requests.get(url)
+    for member in members.json():
+        member_url = member['url']
+        member_data = requests.get(member_url).json()
+        data.append({
+            'name': member_data['login'],
+            'img_url': member_data['avatar_url'],
+            'url': member_data['url'],
+            'bio': member_data['bio'],
+        })
+    return data
 
 
 def index(request):
@@ -35,9 +78,15 @@ def index(request):
     ]
     skills = ['html5', 'css3-alt', 'js-square', 'react', 'node-js', 'npm',
               'python', 'unity', 'android', 'docker', 'php']
+    repos = get_org_repos('mezidia')
+    members = get_org_members('mezidia')
 
     context = {
         'nav_data': nav_data,
-        'skills': skills
+        'skills': skills,
+        'repos': repos,
+        'members': members,
     }
+    print(repos)
+    print(members)
     return render(request, 'main/index.html', context)
