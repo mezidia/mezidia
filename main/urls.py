e9cf3c8d72af0633eba0f992517a8fdbff486aa7
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-02-11 22:24:23 +0200

    Make view

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('form', views.send_email, name='form'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 import requests
 
 from .forms import EmailForm
+
+nav_data = [
+    {'id': '#about', 'text': 'About'},
+    {'id': '#projects', 'text': 'Projects'},
+    {'id': '#members', 'text': 'Members'},
+    {'id': '#skills', 'text': 'Skills'},
+    {'id': '#interests', 'text': 'Interests'},
+    {'id': '#awards', 'text': 'Awards'},
+    {'id': '#newMembers', 'text': 'New members'},
+]
+skills = ['html5', 'css3-alt', 'js-square', 'react', 'node-js', 'npm',
+          'python', 'unity', 'android', 'docker', 'php']
 
 
 def get_org_repos(org_name: str) -> list:
@@ -48,18 +61,34 @@ def get_org_members(org_name: str) -> list:
     return data
 
 
+def send_email(request):
+    sent = False
+    if request.method == 'POST':
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            subject = f"{cd['name']} wants to join Mezidia"
+            message = f"Comments: {cd['comments']}"
+            send_mail(subject, message, cd['email'], ['mezgoodle@gmail.com'])
+            sent = True
+    else:
+        form = EmailForm()
+
+    projects = get_org_repos('mezidia')
+    members = get_org_members('mezidia')
+
+    context = {
+        'form': form,
+        'nav_data': nav_data,
+        'skills': skills,
+        'projects': projects,
+        'members': members,
+        'sent': sent,
+    }
+    return render(request, 'main/index.html', context)
+
+
 def index(request):
-    nav_data = [
-        {'id': '#about', 'text': 'About'},
-        {'id': '#projects', 'text': 'Projects'},
-        {'id': '#members', 'text': 'Members'},
-        {'id': '#skills', 'text': 'Skills'},
-        {'id': '#interests', 'text': 'Interests'},
-        {'id': '#awards', 'text': 'Awards'},
-        {'id': '#newMembers', 'text': 'New members'},
-    ]
-    skills = ['html5', 'css3-alt', 'js-square', 'react', 'node-js', 'npm',
-              'python', 'unity', 'android', 'docker', 'php']
     projects = get_org_repos('mezidia')
     members = get_org_members('mezidia')
 
