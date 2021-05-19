b51bb85bf3a2cac902d633518315124dbc77e1c3
Maxim Zavalniuk <mezidiaofficial@gmail.com>
2021-05-19 11:15:22 +0000

    #2 Create About section

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <h1>Hello</h1>
     <Navbar
       v-bind:items="navbaritems"
     />
+    <div class="container-fluid p-0">
+      <About
+        v-bind:icons="aboutIcons"
+      />
+      <hr class="m-0" />
+    </div>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
+import About from '@/components/About.vue'
 export default {
   name: 'App',
   data () {
@@ -21,22 +27,17 @@ export default {
         { link: '#interests', name: 'Interests' },
         { link: '#awards', name: 'Awards' },
         { link: '#newMembers', name: 'New members' }
+      ],
+      aboutIcons: [
+        { link: 'https://github.com/mezidia', title: 'About', icon_class: 'fab fa-github' },
+        { link: 'https://t.me/sylvenis', title: 'Maxim Telegram profile', icon_class: 'fab fa-telegram' },
+        { link: 'https://www.facebook.com/profile.php?id=100005721694357', title: 'Maxim Facebook profile', icon_class: 'fab fa-facebook-f' }
       ]
     }
   },
   components: {
-    Navbar
+    Navbar,
+    About
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
