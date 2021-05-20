d69cb769d6d1d3ec7dad6d13ed7842fe22fcd343
Maxim Zavalniuk <mezidiaofficial@gmail.com>
2021-05-20 06:37:03 +0000

    #2 Set form sending

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -6,13 +6,35 @@
         have additional questions, ask @mezgoodle in Telegram or write an email which is in <i>About</i>
         section.</p>
       <p class="mb-0">We have no requirements, only <i>desire is important</i>.</p>
-      <form class="pt-3">
+      <form class="pt-3" @submit.prevent="sendEmail">
         <label>Your email</label>
-        <input class="form-control" required type="email" placeholder="name@example.com" v-model="email">
+        <input
+          class="form-control"
+          required
+          type="email"
+          placeholder="name@example.com"
+          v-model="email"
+          name="email"
+          >
         <label>Your name</label>
-        <input class="form-control" required type="text" maxlength="25" placeholder="Enter name" v-model="name">
+        <input
+          class="form-control"
+          required
+          type="text"
+          maxlength="25"
+          placeholder="Enter name"
+          v-model="name"
+          name="name"
+          >
         <label>Your comments</label>
-        <textarea class="form-control" type="text" placeholder="Your comments" rows="3" v-model="comments"></textarea>
+        <textarea
+          class="form-control"
+          type="text"
+          placeholder="Your comments"
+          rows="3"
+          v-model="message"
+          name="message">
+        </textarea>
         <button type="submit" class="btn btn-primary mt-3">Submit</button>
       </form>
     </div>
@@ -20,12 +42,31 @@
 </template>
 
 <script>
+import emailjs from 'emailjs-com'
 export default {
   data () {
     return {
       email: '',
       name: '',
-      comments: ''
+      message: ''
+    }
+  },
+  methods: {
+    sendEmail (e) {
+      try {
+        emailjs.sendForm('service_sp6z45v', 'template_d5pneq8', e.target,
+          'user_TZrOrBt41LsvPCLcZaWB7', {
+            name: this.name,
+            email: this.email,
+            message: this.message
+          })
+      } catch (error) {
+        console.log({ error })
+      }
+      // Reset form field
+      this.name = ''
+      this.email = ''
+      this.message = ''
     }
   }
 }
